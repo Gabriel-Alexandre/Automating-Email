27c2ed46c4ad18a36f559b563b22c0f3874922b5
Gabriel Alexandre Carvalho <82059012+Gabriel-Alexandre@users.noreply.github.com>
2022-01-16 21:08:40 -0300

    update

--- Teste/envia_email_interface.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from interface_email.interface_enviar_email import Interface_Enviar_Email
-
-if __name__ == '__main__':
-    qt = QApplication(sys.argv)
-    interface_enviar_email = Interface_Enviar_Email()
-    interface_enviar_email.show()
-    qt.exec_()
